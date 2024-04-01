59d75bbaf9642112f4a614b39b66a34cb7cc8cd2
Sangyup Yi <sean.yi@hyundai.com>
2024-04-01 10:06:08 +0900

    define macro MY_NDEBUG

--- test_assert/test_assert.cpp
+++ test_assert/test_assert.cpp
@@ -1,13 +1,18 @@
-
-
-///////////////////////////////////////////////////////////////////////////////
-// override assert
+/**
+ * my_assert - macro with one argument that is used inside this for assertions. By default, it is basically defined to be assert, which aborts the program if the assertion is violated. Redefine this macro if you want to do something else, like throwing an exception.
+ */
 #include <cstdlib>   // for abort
 #include <iostream>  // for std::cerr
 #define my_assert(var) do { std::cerr << "assertion failed: " << #var << " in function " << __PRETTY_FUNCTION__ << " at " << __FILE__ << ":" << __LINE__ << std::endl;  abort(); } while(0);
-///////////////////////////////////////////////////////////////////////////////
 
+/**
+ * MY_NDEBUG - disables assertions if defined. Not defined by default, unless the NDEBUG macro is defined (this is a standard C++ macro which disables all asserts).
+ */
 #ifdef NDEBUG
+#define MY_NDEBUG
+#endif
+
+#ifdef MY_NDEBUG
 #define my_plain_assert(x)
 #else
 #include <assert.h>
@@ -17,7 +22,6 @@
 #ifndef my_assert
 #define my_assert(x) my_plain_assert(x)
 #endif
-
 
 int main()
 {
